57e4c7cb18a20c8259a61c1410b32179c441cfbf
Evan Mallery <emallery@vt.edu>
2024-11-14 00:00:53 -0500

    Fix about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <div>
+      <P>
+        This is a silly little hobby project to play with the "Kobold doot" redeem on
+        <a
+          href="https://twitch.tv/sushifur"
+          title="https://twitch.tv/sushifur"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Sushifur</a
+        >'s Twitch stream!
+      </P>
+      <P>
+        Kobold art by
+        <a
+          href="https://www.twitch.tv/posspal"
+          title="https://www.twitch.tv/posspal"
+          target="_blank"
+          rel="noopener noreferrer"
+          >NicOpossum</a
+        >!
+      </P>
+      <P>
+        Website made by
+        <a
+          href="https://twitch.tv/featherderg"
+          title="https://twitch.tv/featherderg"
+          target="_blank"
+          rel="noopener noreferrer"
+          >FeatherDerg</a
+        >.
+      </P>
+    </div>
   </div>
 </template>
 
@@ -10,6 +43,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
+    flex-direction: column;
   }
 }
 </style>
